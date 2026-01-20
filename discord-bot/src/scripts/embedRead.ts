0fd9975cb2ac19b3320ae5e459b338adb91371ab
bigstone <dssong019980106@gmail.com>
2026-01-21 01:16:20 +0900

    little bug fix

--- discord-bot/src/scripts/embedRead.ts
+++ discord-bot/src/scripts/embedRead.ts
@@ -1,5 +1,3 @@
-import { Timestamp } from './../../../backend/node_modules/@angular-devkit/core/node_modules/rxjs/dist/types/internal/types.d';
-import { name } from './../../../backend/node_modules/ci-info/index.d';
 import { Client, GatewayIntentBits, ChannelType, TextChannel } from 'discord.js';
 import dotenv from 'dotenv';
 
