ce3f172353901cac0e4fed61506571a2cdff7a78
Your Name <you@example.com>
2026-01-21 10:52:45 +0900

    remove express cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,21 +7,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS 설정
-  app.enableCors({
-    origin: [
-      'http://localhost:8080',
-      'https://localhost:8080',
-      'http://boogibooks.com',
-      'https://boogibooks.com',
-      'http://www.boogibooks.com',
-      'https://www.boogibooks.com',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id'],
-  });
-
   await app.listen(process.env.PORT ?? 3000, () =>
     console.log(`Server is running on port ${process.env.PORT ?? 3000}`),
   );
