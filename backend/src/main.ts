a40f1c6728f9ffc824b7cb6d4a1604f2b59e5b4f
bigstone <dssong019980106@gmail.com>
2026-01-30 00:37:57 +0900

    non-dev condition

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
   await app.listen(process.env.PORT ?? 3000, () =>
     console.log(`Server is running on port ${process.env.PORT ?? 3000}`),
   );
